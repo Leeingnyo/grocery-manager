9cf53dd98aae234dc644a9c173d965b9dfdf9014
Leeingnyo <dyd1928@naver.com>
2025-01-10 13:59:44 +0900

    Add sections in list grocery

--- add-grocery.js
+++ add-grocery.js
@@ -3,7 +3,9 @@ const { el } = redom;
 export class AddGrocery {
   constructor() {
     this.el = el('section.add-grocery',
-      this.button = el('button', 'Back to Grocery List'),
+      el('nav.flex.flex-wrap.items-center.gap-2',
+        this.button = el('button.btn', 'Back to Grocery List'),
+      ),
     );
 
     this.button.addEventListener('click', () => {
--- list-grocery.js
+++ list-grocery.js
@@ -1,13 +1,61 @@
-const { el } = redom;
+const { el, list } = redom;
+
+class Item {
+  #name;
+
+  constructor() {
+    this.el = el('article',
+      this.#name = el('span'),
+    );
+  }
+
+  update({ name }) {
+    this.#name.textContext = name;
+  }
+}
+
+class LargeSection {
+  #name;
+  #items;
+
+  constructor() {
+    this.el = el('section.my-2',
+      this.#name = el('h2.text-2xl'),
+      this.#items = list('section', Item),
+    );
+  }
+
+  update({ id, name = '' } = {}, _index, _items, context) {
+    this.#name.textContent = name;
+    this.#items.update(context?.[id] ?? []);
+  }
+}
+
+const largeSections = [
+  { id: 0, name: '냉장고' },
+  { id: 1, name: '냉동고' },
+  { id: 2, name: '실온' },
+];
 
 export class ListGrocery {
+  #largeSection;
+
   constructor() {
     this.el = el('section.list-grocery',
-      this.button = el('button', 'Add Grocery'),
+      el('nav.flex.flex-wrap.items-center.gap-2',
+        this.button = el('button.btn', 'Add Grocery'),
+        el('button.btn', 'Add Item'),
+        el('button.btn', 'Manage Items'),
+        el('button.btn', 'Shopping List'),
+      ),
+      this.#largeSection = list('section', LargeSection),
     );
 
     this.button.addEventListener('click', () => {
       page('/add-grocery');
     });
+
+    this.#largeSection.update(largeSections, {});
   }
 }
+
