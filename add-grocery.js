478d15abe3a6e5753bb5b9b6eabc283f0042264b
Leeingnyo <dyd1928@naver.com>
2025-01-15 19:07:46 +0900

    Use anchor tag instead of click event

--- add-grocery.js
+++ add-grocery.js
@@ -4,13 +4,9 @@ export class AddGrocery {
   constructor() {
     this.el = el('section.add-grocery',
       el('nav.flex.flex-wrap.items-center.gap-2',
-        this.button = el('button.btn', '물품 목록'),
+        el('a', { href: '/grocery' }, el('button.btn', '물품 목록')),
       ),
     );
-
-    this.button.addEventListener('click', () => {
-      page('/grocery');
-    });
   }
 }
 
--- list-grocery.js
+++ list-grocery.js
@@ -26,17 +26,13 @@ export class ListGrocery {
   constructor() {
     this.el = el('section.list-grocery',
       el('nav.flex.flex-wrap.items-center.gap-2',
-        this.button = el('button.btn', '물품 추가'),
+        el('a', { href: '/add-grocery' }, el('button.btn', '물품 추가')),
         el('button.btn', '템플릿 추가'),
         el('button.btn', '템플릿 관리'),
         el('button.btn', '장보기'),
       ),
       this.#largeSection = list('section', LargeSection),
     );
-
-    this.button.addEventListener('click', () => {
-      page('/add-grocery');
-    });
 
     stateEmitter$.subscribe((state) => {
       this.#largeSection.update(state.largeSections, state);
